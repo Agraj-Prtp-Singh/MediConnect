cdc595fd4959d8326e7a0a9649da879fdd5415f9
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-08-11 21:00:40 +0545

    feat: add database connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({
