8c71e784c66fd7a3a075d72ea89c9e7053fefedc
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-08-13 21:52:37 +0545

    refactor: change context for better auth

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,15 +1,48 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import api from "../services/api";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const response = await api.get("/users/me");
+
+        setUser(response.data.user);
+      } catch (error) {
+        localStorage.removeItem("token");
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadUser();
+  }, []);
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
 
   return (
     <AuthContext.Provider
       value={{
         user,
         setUser,
+        logout,
+        loading,
       }}
     >
       {children}
