9969c8f213f072b4fdc699e0b826d0480166c5e8
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-08-13 21:54:22 +0545

    refactor: update the user route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const User = require("../models/User");
 const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.get("/me", protect, (req, res) => {
-  res.json({
-    message: "You are authenticated",
-    user: req.user,
-  });
+router.get("/me", protect, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({git status
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
 });
 
 module.exports = router;
